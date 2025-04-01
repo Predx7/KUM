b80e0db9ac68d5eb092786ae070e34fb923a4099
Predx7 <77452258+Predx7@users.noreply.github.com>
2025-04-01 16:47:32 +0300

    Delete utils directory

--- utils/supabase.js
+++
@@ -1,9 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://zehjecgkpjnmnbfqlkba.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplaGplY2drcGpubW5iZnFsa2JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyNTA0NzAsImV4cCI6MjA1ODgyNjQ3MH0.uWSy84K8j4UMxsjDnx0ZReAymxfOb96fzINH_P3LnSo';
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export default supabase;
